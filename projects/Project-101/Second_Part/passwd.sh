ba3f93822fc5ee3cd134bede0726f3381495bac0
latif <lyildirim.3017@gmail.com>
2023-02-07 16:20:35 +0100

    .

---
+++ projects/Project-101/Second_Part/passwd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Kullanic adi :" username
+read -p "Parola" password  
+
+adduser $username
+echo "$password" | passwd --stdin $username
+
+chage -d 0 $username
+echo -e "Kullanici adi: $username\nParola: $password"  
--- projects/Project-101/Second_Part/user_password.sh
+++ projects/Project-101/Second_Part/user_password.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 read -p "Kullanic adi :" username
-read -p "Parola" password  
+read -p "Parola :" password
+# read -p "Aciklama :" comment 
 
-adduser $username
-echo "$password" | passwd --stdin $username
+useradd -c "$comment" -m $username
+t="$username:$password:1007:1007:/home/$username:/bin/bash"
+echo "$t" > a.txt
+sudo newusers a.txt
+sleep 1
+rm a.txt
 
 chage -d 0 $username
-echo -e "Kullanici adi: $username\nParola: $password"  
+echo -e "Kullanici adi: $username\nParola: $password" 
