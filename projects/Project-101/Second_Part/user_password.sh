5c28c6c2567c79e4a721b1bb9938ae93655e86df
latif <lyildirim.3017@gmail.com>
2023-02-07 16:22:18 +0100

    #!/usr/bin/env bash

--- projects/Project-101/Second_Part/user_password.sh
+++ projects/Project-101/Second_Part/user_password.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 read -p "Kullanic adi :" username
 read -p "Parola :" password
 # read -p "Aciklama :" comment 
 
-useradd -c "$comment" -m $username
-t="$username:$password:1007:1007:/home/$username:/bin/bash"
-echo "$t" > a.txt
-sudo newusers a.txt
-sleep 1
-rm a.txt
+passwd ${username} << EOD
+${password}
+${password}
+EOD
 
 chage -d 0 $username
 echo -e "Kullanici adi: $username\nParola: $password" 
