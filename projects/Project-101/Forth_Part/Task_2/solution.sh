fda5bb3e485307067c4cc15fd203265c52efd8df
latif <lyildirim.3017@gmail.com>
2023-02-13 15:36:45 +0100

    task2 modification

---
+++ projects/Project-101/Forth_Part/Task_2/solution.sh
@@ -0,0 +1,6 @@
+#!/bash/bin
+KEY=$(cat certificate.pem)
+sed -e 's/\\n/\n/g' ${KEY}> new.pem
+
+# echo -e $(KEY) > new.pem # Another way to solve it
+# cat certificate.pem | sed -e 's/\\n/\n/g' # Another way to solve it
