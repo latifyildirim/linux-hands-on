454aa2515b052d07b13d44c2a564c538336a61c1
latif <lyildirim.3017@gmail.com>
2023-02-13 09:55:56 +0100

    script düzenlendi

--- projects/Project-101/First_Part/script.sh
+++ projects/Project-101/First_Part/script.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-grep -i 0c127ab5cdf997cf4 event_history.csv | grep -i Terminate | egrep -o 'i-[[:xdigit:]]+' | sort | uniq >  ./result.txt
+while true
+do
+    read -p 'Please write your file location: ' file
+    if [[ $FILE == '' ]]
+    then
+        echo "Please give a file"
+    elif [[ ! -f $FILE ]]
+    then
+        echo "Please give an existing file"
+    else
+        break
+done
 
-grep -i privatIp event_history.csv | grep -i Terminate | egrep -o 'i-[[:xdigit:]]+' | sort | uniq >  ./result.txt
+# grep -i 0c127ab5cdf997cf4 event_history.csv | grep -i Terminate | egrep -o 'i-[[:xdigit:]]+' | sort | uniq >  ./result.txt
 
-grep -i privatIp event_history.csv | grep -i Terminate | grep -Eo 'i-.{17}' | sort | uniq >  ./result.txt
+# grep -i privatIp event_history.csv | grep -i Terminate | egrep -o 'i-[[:xdigit:]]+' | sort | uniq >  ./result.txt
+
+# grep -i privatIp event_history.csv | grep -i Terminate | grep -Eo 'i-.{17}' | sort | uniq >  ./result.txt
+
+cat $FILE | grep serdar event_history.csv | grep -i Terminate | grep -Eo 'i-[a-zA-Z0-9]{17}' | sort | uniq >  /tmp/result.txt
+echo "Your result is ready under the /tmp/result.txt file"
